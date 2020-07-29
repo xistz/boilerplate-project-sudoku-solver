3d913d10ec7bba99efc86d71f83587f1e858b8d0
Jonathan Chan <tmxistz@gmail.com>
2020-07-29 16:57:05 +0900

    completed InputValid()

--- public/sudoku-solver.js
+++ public/sudoku-solver.js
@@ -1,9 +1,40 @@
 const textArea = document.getElementById('text-input');
 // import { puzzlesAndSolutions } from './puzzle-strings.js';
 
+const updateGrid = (puzzle) => {
+  [...puzzle].forEach((char, index) => {
+    const number = parseInt(char);
+
+    if (number) {
+      setCell(number, index);
+    }
+  });
+};
+
+const InputValid = (input) => {
+  return /^[1-9]$/.test(input);
+};
+
+const PuzzleValid = (puzzleString) => {
+  return /^([1-9]|\.){81}$/.test(puzzleString);
+};
+
+const setCell = (number, index) => {
+  const row = Math.floor(index / 9);
+  const column = (index % 9) + 1;
+  const cell = String.fromCharCode(row + 65) + column;
+
+  const td = document.getElementById(cell);
+  td.value = number;
+};
+
 document.addEventListener('DOMContentLoaded', () => {
   // Load a simple puzzle into the text area
-  textArea.value = '..9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6..';
+  const puzzle =
+    '..9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6..';
+
+  textArea.value = puzzle;
+  updateGrid(puzzle);
 });
 
 /* 
@@ -13,6 +44,7 @@ document.addEventListener('DOMContentLoaded', () => {
 */
 try {
   module.exports = {
-
-  }
+    PuzzleValid,
+    InputValid,
+  };
 } catch (e) {}
--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -16,75 +16,88 @@ let Solver;
 suite('UnitTests', () => {
   suiteSetup(() => {
     // Mock the DOM for testing and load Solver
-    return JSDOM.fromFile('./views/index.html')
-      .then((dom) => {
-        global.window = dom.window;
-        global.document = dom.window.document;
+    return JSDOM.fromFile('./views/index.html').then((dom) => {
+      global.window = dom.window;
+      global.document = dom.window.document;
 
-        Solver = require('../public/sudoku-solver.js');
-      });
+      Solver = require('../public/sudoku-solver.js');
+    });
   });
-  
+
   // Only the digits 1-9 are accepted
   // as valid input for the puzzle grid
-  suite('Function ____()', () => {
+  suite('Function InputValid()', () => {
     test('Valid "1-9" characters', (done) => {
-      const input = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
+      const inputs = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
-      // done();
+      inputs.forEach((input) => {
+        assert.isTrue(Solver.InputValid(input));
+      });
+
+      done();
     });
 
-    // Invalid characters or numbers are not accepted 
+    // Invalid characters or numbers are not accepted
     // as valid input for the puzzle grid
     test('Invalid characters (anything other than "1-9") are not accepted', (done) => {
-      const input = ['!', 'a', '/', '+', '-', '0', '10', 0, '.'];
+      const inputs = ['!', 'a', '/', '+', '-', '0', '10', 0, '.'];
 
-      // done();
+      inputs.forEach((input) => {
+        assert.isFalse(Solver.InputValid(input));
+      });
+
+      done();
     });
   });
-  
+
   suite('Function ____()', () => {
-    test('Parses a valid puzzle string into an object', done => {
-      const input = '..9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6..';
-      
+    test('Parses a valid puzzle string into an object', (done) => {
+      const input =
+        '..9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6..';
+
+      assert.isTrue(Solver.PuzzleValid(input));
+      done();
       // done();
     });
-    
-    // Puzzles that are not 81 numbers/periods long show the message 
+
+    // Puzzles that are not 81 numbers/periods long show the message
     // "Error: Expected puzzle to be 81 characters long." in the
     // `div` with the id "error-msg"
-    test('Shows an error for puzzles that are not 81 numbers long', done => {
+    test('Shows an error for puzzles that are not 81 numbers long', (done) => {
       const shortStr = '83.9.....6.62.71...9......1945....4.37.4.3..6..';
-      const longStr = '..9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6...';
+      const longStr =
+        '..9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6...';
       const errorMsg = 'Error: Expected puzzle to be 81 characters long.';
       const errorDiv = document.getElementById('error-msg');
-      
+
       // done();
     });
   });
 
   suite('Function ____()', () => {
     // Valid complete puzzles pass
-    test('Valid puzzles pass', done => {
-      const input = '769235418851496372432178956174569283395842761628713549283657194516924837947381625';
+    test('Valid puzzles pass', (done) => {
+      const input =
+        '769235418851496372432178956174569283395842761628713549283657194516924837947381625';
 
       // done();
     });
 
     // Invalid complete puzzles fail
-    test('Invalid puzzles fail', done => {
-      const input = '779235418851496372432178956174569283395842761628713549283657194516924837947381625';
+    test('Invalid puzzles fail', (done) => {
+      const input =
+        '779235418851496372432178956174569283395842761628713549283657194516924837947381625';
 
       // done();
     });
   });
-  
-  
+
   suite('Function ____()', () => {
     // Returns the expected solution for a valid, incomplete puzzle
-    test('Returns the expected solution for an incomplete puzzle', done => {
-      const input = '..9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6..';
-      
+    test('Returns the expected solution for an incomplete puzzle', (done) => {
+      const input =
+        '..9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6..';
+
       // done();
     });
   });
